e67ead954be21180064aa777f612af42848ccf50
ms.alievaalya@gmail.com <ms.alievaalya@gmail.com>
2022-06-12 14:46:00 +0300

    Methods out of class

--- Academy/main.cpp
+++ Academy/main.cpp
@@ -15,48 +15,59 @@ protected:
 	string first_name;
 	unsigned int age;
 public:
-	const string& get_last_name()const
-	{
-		return last_name;
-	}
-	const string& get_first_name()const
-	{
-		return first_name;
-	}
-	unsigned int get_age()const
-	{
-		return age;
-	}
-	void set_last_name(const string& last_name)
-	{
-		this->last_name = last_name;
-	}
-	void set_first_name(const string& first_name)
-	{
-		this->first_name = first_name;
-	}
-	void set_age(unsigned int age)
-	{
-		this->age = age;
-	}
+	const string& get_last_name()const;
+	const string& get_first_name()const;
+	unsigned int get_age()const;
+	void set_last_name(const string& last_name);
+	void set_first_name(const string& first_name);
+	void set_age(unsigned int age);
 	//					Constructors:
-	Human(HUMAN_TAKE_PARAMETERS)
-	{
-		set_last_name(last_name);
-		set_first_name(first_name);
-		set_age(age);
-		cout << "HConstructor:\t" << this << endl;
-	}
-	virtual~Human()
-	{
-		cout << "HDestructor:\t" << this << endl;
-	}
+	Human(HUMAN_TAKE_PARAMETERS);
+	virtual~Human();
 	//				Methods:
-	virtual void print()const //без virtual раннее связывание 
-	{
-		cout << last_name << " " << first_name << " " << age << "years.\n";
-	}
+	virtual void print()const;
 };
+const string& Human::get_last_name()const
+{
+	return last_name;
+}
+const string& Human::get_first_name()const
+{
+	return first_name;
+}
+unsigned int Human::get_age()const
+{
+	return age;
+}
+void Human::set_last_name(const string& last_name)
+{
+	this->last_name = last_name;
+}
+void Human::set_first_name(const string& first_name)
+{
+	this->first_name = first_name;
+}
+void Human::set_age(unsigned int age)
+{
+	this->age = age;
+}
+//					Constructors:
+Human::Human(HUMAN_TAKE_PARAMETERS)
+{
+	set_last_name(last_name);
+	set_first_name(first_name);
+	set_age(age);
+	cout << "HConstructor:\t" << this << endl;
+}
+ Human::~Human()
+{
+	cout << "HDestructor:\t" << this << endl;
+}
+//				Methods:
+ void Human::print()const //без virtual раннее связывание 
+{
+	cout << last_name << " " << first_name << " " << age << "years.\n";
+}
 ostream& operator<<(ostream& os, const Human& obj)
 {
 	return
@@ -73,68 +84,84 @@ class Student :public Human
 	double rating;
 	double attendance;
 public:
-	const string& get_specialty()const
-	{
-		return specialty;
-	}
-	const string& get_group()const
-	{
-		return group;
-	}
-	unsigned int  get_year()const
-	{
-		return year;
-	}
-	double get_rating()const
-	{
-		return rating;
-	}
-	double get_attendance()const
-	{
-		return attendance;
-	}
-	void set_specialty(const string& specialty)
-	{
-		this->specialty = specialty;
-	}
-	void set_group(const string& group)
-	{
-		this->group = group;
-	}
-	void set_year(unsigned int year )
-	{
-		this->year = year;
-	}
-	void set_rating(double rating)
-	{
-		this->rating = rating;
-	}
-	void set_attendance(double attendance)
-	{
-		this->attendance = attendance;
-	}
+	const string& get_specialty()const;
+	const string& get_group()const;
+	unsigned int  get_year()const;
+	double get_rating()const;
+	double get_attendance()const;
+	void set_specialty(const string& specialty);
+	void set_group(const string& group);
+	void set_year(unsigned int year);
+	void set_rating(double rating);
+	void set_attendance(double attendance);
 	//				Constructors:
 	Student
-	(HUMAN_TAKE_PARAMETERS, STUDENT_TAKE_PATAMETERS):Human(HUMAN_GIVE_PARAMETERS)	//Dелегируем конструктор Human класса
-	{
-		set_specialty(specialty);
-		set_group(group);
-		set_year(year);
-		set_rating(rating);
-		set_attendance(attendance);
-		cout << "SConstructor:\t" << this << endl;
-	}
-	~Student()
-	{
-		cout << "SDestructor:\t" << this << endl;
-	}
+	(HUMAN_TAKE_PARAMETERS, STUDENT_TAKE_PATAMETERS);
+	~Student();
 	//				Methods:
-	void print()const
-	{
-		Human::print();
-		cout << specialty + " " + group << " " << year << " " << rating << " " << attendance << endl;
-	}
+	void print()const;
 };
+const string& Student::get_specialty()const
+{
+	return specialty;
+}
+const string& Student::get_group()const
+{
+	return group;
+}
+unsigned int  Student::get_year()const
+{
+	return year;
+}
+double Student::get_rating()const
+{
+	return rating;
+}
+double Student::get_attendance()const
+{
+	return attendance;
+}
+void Student::set_specialty(const string& specialty)
+{
+	this->specialty = specialty;
+}
+void Student::set_group(const string& group)
+{
+	this->group = group;
+}
+void Student::set_year(unsigned int year)
+{
+	this->year = year;
+}
+void Student::set_rating(double rating)
+{
+	this->rating = rating;
+}
+void Student::set_attendance(double attendance)
+{
+	this->attendance = attendance;
+}
+//				Constructors:
+Student::Student
+(HUMAN_TAKE_PARAMETERS, STUDENT_TAKE_PATAMETERS) :Human(HUMAN_GIVE_PARAMETERS)	//Dелегируем конструктор Human класса
+{
+	set_specialty(specialty);
+	set_group(group);
+	set_year(year);
+	set_rating(rating);
+	set_attendance(attendance);
+	cout << "SConstructor:\t" << this << endl;
+}
+Student::~Student()
+{
+	cout << "SDestructor:\t" << this << endl;
+}
+//				Methods:
+void Student::print()const
+{
+	Human::print();
+	cout << specialty + " " + group << " " << year << " " << rating << " " << attendance << endl;
+}
 ostream& operator<<(ostream& os, const Student& obj)
 {
 	/*os << (Human&)obj;*/
@@ -148,47 +175,61 @@ class Teacher :public Human
 	string speciality;
 	unsigned int experience;
 public:
-	const string& get_speciality()const
-	{
-		return speciality;
-	}
-	unsigned int get_experience()const
-	{
-		return experience;
-	}
-	void set_speciality(const string& speciality)
-	{
-		this->speciality = speciality;
-	}
-	void set_experience(unsigned int experience)
-	{
-		this->experience = experience;
-	}
+	const string& get_speciality()const;
+	unsigned int get_experience()const;
+	void set_speciality(const string& speciality);
+	void set_experience(unsigned int experience);
 	//					Constructors:
 	Teacher
 	(
 		const string& last_name, const string& first_name, unsigned int age,
 		const string& speciality, unsigned int experience
-	) :Human(last_name, first_name, age)
-	{
-		set_speciality(speciality);
-		set_experience(experience);
-		cout << "TConstructor:\t" << this << endl;
-	}
-	~Teacher()
-	{
-		cout << "TDestructor:\t" << this << endl;
-	}
+	);
+	~Teacher();
 	
 	//				Methods:
- void print()const  
-	{
-		{
-			Human::print();
-			cout << "speciality\t" << speciality + " " << "experience\t" << experience << endl;
-		}
-	}
+	void print()const;
 };
+const string& Teacher::get_speciality()const
+{
+	return speciality;
+}
+unsigned int Teacher::get_experience()const
+{
+	return experience;
+}
+void Teacher::set_speciality(const string& speciality)
+{
+	this->speciality = speciality;
+}
+void Teacher::set_experience(unsigned int experience)
+{
+	this->experience = experience;
+}
+//					Constructors:
+Teacher::Teacher
+(
+	const string& last_name, const string& first_name, unsigned int age,
+	const string& speciality, unsigned int experience
+) :Human(last_name, first_name, age)
+{
+	set_speciality(speciality);
+	set_experience(experience);
+	cout << "TConstructor:\t" << this << endl;
+}
+Teacher::~Teacher()
+{
+	cout << "TDestructor:\t" << this << endl;
+}
+
+//				Methods:
+void Teacher::print()const
+{
+	{
+		Human::print();
+		cout << "speciality\t" << speciality + " " << "experience\t" << experience << endl;
+	}
+}
 ostream& operator<<(ostream& os, const Teacher& obj)
 {
 	return os<<(Human&)obj
@@ -199,36 +240,48 @@ class Graduate :public Student
 {
 	string diploma;
 public:
-	const string& get_diploma()const
-	{
-		return diploma;
-	}
-	void set_diploma(const string& diploma)
-	{
-		this->diploma = diploma;
-	}
+	const string& get_diploma()const;
+	void set_diploma(const string& diploma);
 	//			Constructors:
 	Graduate
 	(
 		const string& last_name, const string& first_name, unsigned int age,
 		const string& speciality, const string& group, unsigned int year, double rating, double attendance,
 		const string& diploma
-	) :Student(last_name, first_name, age,speciality, group, year, rating, attendance)
-	{
-		set_diploma(diploma);
-		cout << "GConstructor:\t" << this << endl;
-	}
-	~Graduate()
-	{
-		cout << "GDestructor:\t" << this << endl;
-	}
+	);
+	~Graduate();
 	//					Methods:
-	void print()const
-	{
-		Student::print();
-		cout << "Тема диплома: " << diploma << endl;
-	}
+	void print()const;
 };
+const string& Graduate::get_diploma()const
+{
+	return diploma;
+}
+void Graduate::set_diploma(const string& diploma)
+{
+	this->diploma = diploma;
+}
+//			Constructors:
+Graduate::Graduate
+(
+	const string& last_name, const string& first_name, unsigned int age,
+	const string& speciality, const string& group, unsigned int year, double rating, double attendance,
+	const string& diploma
+) :Student(last_name, first_name, age, speciality, group, year, rating, attendance)
+{
+	set_diploma(diploma);
+	cout << "GConstructor:\t" << this << endl;
+}
+Graduate::~Graduate()
+{
+	cout << "GDestructor:\t" << this << endl;
+}
+//					Methods:
+void Graduate::print()const
+{
+	Student::print();
+	cout << "Тема диплома: " << diploma << endl;
+}
 ostream& operator<<(ostream& os, const Graduate& obj)
 {
 	return os << (Student&)obj << obj.get_diploma();
